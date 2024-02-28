2df0a7d93d4da3420e75ca8c18ff811c8564c6a5
zohai-ibb <johaibansari002@gmail.com>
2024-02-28 16:34:17 +0530

    sign in api endpoint Completd

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res, next) => {
@@ -12,3 +13,19 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+
+export const signin = async (req, res, next) =>{
+  const {email, password} = req.body;
+  try{
+    const validUser = await User.findOne({ email });
+    if(!validUser) return next(errorHandler(404, "User Not Found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if(!validPassword) return next(errorHandler(401, "Invalid Credentials!"));
+    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+    const {password: pass, ...rest } = validUser._doc;
+    res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest);
+  }catch(error){
+    next(error);
+  }
+}
