5c6f13912fd1a14687e5c1634bc98058ad760874
zohai-ibb <johaibansari002@gmail.com>
2024-03-02 23:24:53 +0530

    Added redux persist to store the current user in local storage so that even after refresh user stay signed in

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice';
+import storage from 'redux-persist/lib/storage';
+import persistStore from 'redux-persist/es/persistStore';
+import persistReducer from 'redux-persist/es/persistReducer';
+
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persitstConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persitstConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {user: userReducer},
+  reducer: persistedReducer,
     middleware: (getDefaultMiddleware) => 
         getDefaultMiddleware({
             serializableCheck: false,
         }),
 })
+
+export const persistor = persistStore(store)
