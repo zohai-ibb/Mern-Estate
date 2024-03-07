37d6ce4a521d8c8a99f9becc612b8cc58bab78cb
zohai-ibb <johaibansari002@gmail.com>
2024-03-07 19:52:02 +0530

    Front end updated

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -33,7 +33,7 @@ export default function OAuth() {
     }
 
     return (
-        <div>
+        <div className='flex flex-col gap-4'>
             <button onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>Continue with google</button>
         </div>
     )
