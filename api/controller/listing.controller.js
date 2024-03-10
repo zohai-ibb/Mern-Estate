3f118698bc71c1f4769d2cd523797f21a1b5aa72
zohai-ibb <johaibansari002@gmail.com>
2024-03-10 16:17:58 +0530

    complete update listing fucntionality

--- api/controller/listing.controller.js
+++ api/controller/listing.controller.js
@@ -44,10 +44,22 @@ export const updateListing = async (req, res, next) => {
       req.body,
       { new: true }
     )
-    res.status(200).json('Listing Updated Successfully')
+    res.status(200).json(updatedListing)
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getListing = async (req, res, next) =>{
+
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if(!listing) return next(errorHandler(404, 'Listing Not Found'))
+    res.status(200).json(listing);
   } catch (error) {
     next(error);
   }
 
+  
 
-};
+}
